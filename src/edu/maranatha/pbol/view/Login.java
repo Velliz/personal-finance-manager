d5db16a856b69c817f7ac1a19abd20863d526156
uiieBrahmana <dwipaulina@windowslive.com>
2016-05-23 22:46:42 +0700

    ireport lib final

--- src/edu/maranatha/pbol/view/Login.java
+++ src/edu/maranatha/pbol/view/Login.java
@@ -15,6 +15,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.text.ParseException;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,6 +23,13 @@ import javax.swing.ImageIcon;
 import javax.swing.Timer;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.table.DefaultTableModel;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRTableModelDataSource;
+import net.sf.jasperreports.view.JasperViewer;
 import org.hibernate.Session;
 
 /**
@@ -31,6 +39,7 @@ import org.hibernate.Session;
 public class Login extends javax.swing.JFrame implements ActionListener {
 
     private Timer time = new Timer(1000, this);
+    private DefaultTableModel tableModel;
 
     /**
      * Creates new form Login
@@ -41,6 +50,18 @@ public class Login extends javax.swing.JFrame implements ActionListener {
         initComponents();
         setLocationRelativeTo(null);
         time.start();
+        
+        JasperPrint jasperPrint = null;
+        TableModelData();
+        try {
+            JasperCompileManager.compileReportToFile("report/balancesheet.jrxml");
+            jasperPrint = JasperFillManager.fillReport("report/balancesheet.jasper", new HashMap(),
+                    new JRTableModelDataSource(tableModel));
+            JasperViewer jasperViewer = new JasperViewer(jasperPrint);
+            jasperViewer.setVisible(true);
+        } catch (JRException ex) {
+            ex.printStackTrace();
+        }
     }
 
     /**
@@ -257,4 +278,22 @@ public class Login extends javax.swing.JFrame implements ActionListener {
         int s = now.get(Calendar.SECOND);
         dates.setText(h + ":" + m + ":" + s);
     }
+    
+    private void TableModelData() {
+        String[] columnNames = {"Id", "Name", "Department", "Email"};
+        String[][] data = {
+            {"111", "G Conger", " Orthopaedic", "jim@wheremail.com"},
+            {"222", "A Date", "ENT", "adate@somemail.com"},
+            {"333", "R Linz", "Paedriatics", "rlinz@heremail.com"},
+            {"444", "V Sethi", "Nephrology", "vsethi@whomail.com"},
+            {"555", "K Rao", "Orthopaedics", "krao@whatmail.com"},
+            {"666", "V Santana", "Nephrology", "vsan@whenmail.com"},
+            {"777", "J Pollock", "Nephrology", "jpol@domail.com"},
+            {"888", "H David", "Nephrology", "hdavid@donemail.com"},
+            {"999", "P Patel", "Nephrology", "ppatel@gomail.com"},
+            {"101", "C Comer", "Nephrology", "ccomer@whymail.com"}
+        };
+        tableModel = new DefaultTableModel(data, columnNames);
+    }
+   
 }
